9b919fe565e65a07be2f0711212dd994145ede28
Ricardo <Ricardo@CEPHEUS-WIN>
2023-07-09 14:51:34 +0100

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CaliTrack.Data 
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
---
+++ Models/ExerciseCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaliTrack.Models
+{
+    public class ExerciseCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
